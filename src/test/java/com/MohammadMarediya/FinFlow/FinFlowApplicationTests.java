95777555370eaaa9452d494ec34d52af94851d32
mohammad07779 <mohammedmmarediya@gmail.com>
2025-08-04 23:24:50 -0700

    feat:add full initial project structure with required dependency

---
+++ src/test/java/com/MohammadMarediya/FinFlow/FinFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MohammadMarediya.FinFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
